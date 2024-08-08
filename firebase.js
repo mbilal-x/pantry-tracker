8ebfae9760e01f74f97334235965bc54ac18640a
mbilal-x <muhammadbx4@gmail.com>
2024-08-08 21:44:11 +0500

    env key

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from 'firebase/analytics';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCOZpyreoaO1mSjVz2u7fovHgp2YhDnjtk',
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: 'panty-tracker.firebaseapp.com',
   projectId: 'panty-tracker',
   storageBucket: 'panty-tracker.appspot.com',
   messagingSenderId: '46022386637',
   appId: '1:46022386637:web:e49ec341691817a4e91a84',
-  measurementId: 'G-EPRQZTH8WL',
 };
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-const analytics = getAnalytics(app);
-export { firestore, analytics };
+export { firestore };
